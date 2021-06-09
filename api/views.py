1c6e83e98ee12122387a8a19e3dd83daa5e53c03
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-06-09 22:50:29 +0530

    GS1_Basic_of_Serializer_&_JSON

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse,JsonResponse
+#Model Object - Single Student Data
 
-# Create your views here.
+def student_detail(request,pk):
+    stu = Student.objects.get(id=pk)         #Calling Data from Student Model
+    serializer = StudentSerializer(stu)     #Converting to Serialied data
+    # these two lines to single line
+    # json_data = JSONRenderer().render(serializer.data)  #Converting serialized data into JSON data
+    # return HttpResponse(json_data,content_type='application/json')
+    return JsonResponse(serializer.data)
+
+
+#All Student Data    
+def student_list(request):
+    stu = Student.objects.all()         #Calling Data from Student Model
+    serializer = StudentSerializer(stu,many=True)     #Converting to Serialied data
+    # These two lines to single line
+    # json_data = JSONRenderer().render(serializer.data)  #Converting serialized data into JSON data
+    # return HttpResponse(json_data,content_type='application/json')
+    return JsonResponse(serializer.data,safe=False) #for multiple data safe= False defaut safe=True
