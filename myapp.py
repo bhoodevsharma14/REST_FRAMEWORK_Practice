38d8e6908506ff602180769423db243305abdb38
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-06-10 10:13:28 +0530

    Saving Data into Models By Json data

--- myapp.py
+++ myapp.py
@@ -1,6 +1,16 @@
 import requests
+import json
 
-URL = 'http://127.0.0.1:8000/stuinfo/1'
-r = requests.get(url = URL)
+
+URL = 'http://127.0.0.1:8000/stucreate/'
+
+data = {
+    'name':'Minakshi Upadhyay',
+    'roll':104,
+    'city':'Mandsaur'
+}
+json_data = json.dumps(data)
+# r = requests.get(url = URL)
+r = requests.post(url = URL,data=json_data)
 data = r.json()
 print(data)
